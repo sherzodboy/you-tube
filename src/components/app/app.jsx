69eba215c5cce6237c553acdf2ad23d48a133298
sherzod02 <diyorbekolimov2000@gmail.com>
2022-11-28 15:58:51 +0500

    Components added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,18 @@
-import { Stack, Button, Container } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Main, Channel, Navbar, VideoDetail, Search } from "../";
 
 const App = () => {
   return (
-    <Container>
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
-    </Container>
+    <Box>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/channel/:id" element={<Channel />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/search/:id" element={<Search />} />
+      </Routes>
+    </Box>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "./components";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { App } from "./components";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
