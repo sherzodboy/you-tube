c7e9fae662f2968e4282f64385411fef5ef42979
sherzod02 <diyorbekolimov2000@gmail.com>
2022-11-29 21:45:53 +0500

    Added navbar

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Channel } from "./channel/channel";
 export { default as Navbar } from "./navbar/navbar";
 export { default as VideoDetail } from "./video-detail/video-detail";
 export { default as Search } from "./search/search";
+export { default as searchBar } from "./search-bar/search-bar";
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const logo =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpr1UZWFBzbwHoRTjX7QzjoU1kgDA0WfjLHg&usqp=CAU";
